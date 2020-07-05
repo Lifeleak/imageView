62b71601b149f77cb02f3e2952b3005686bbab72
Seong Cheol Woo <woosc1121@gmail.com>
2020-07-04 20:57:30 -0700

    03*4

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    
+    @IBOutlet var btnResize: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+    }
+    
 }
 
