0504b1e1db6cc0fc41a7eb3ddbb5350b6cc3e24a
Seong Cheol Woo <woosc1121@gmail.com>
2020-07-04 20:37:48 -0700

    Initial Commit

---
+++ imageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageView
+//
+//  Created by Seong Cheol Woo on 7/4/20.
+//  Copyright © 2020 Seong Cheol Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
